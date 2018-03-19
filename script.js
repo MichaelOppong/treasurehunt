1dd7048bbcb38cc52f9ae3f59f9ebc4c20099fc2
Ellen Watkins <ellen@scripted.org>
2018-03-19 16:58:55 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+$("#revelio").click(function(){
+    $("#frog").slideToggle();
+});
+
+$("#aparacium").click(function(){
+    $("#ink").fadeToggle();
+});
+
+$("#expecto").click(function(){
+    $("#dementor").toggle();
+});
